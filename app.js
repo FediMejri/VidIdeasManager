2ffd11ae32381ef4a1ccec5ab10ab38e3e6a2798
FediMejri <mejri.fedi.95@gmail.com>
2020-11-11 14:30:30 +0100

    version 1.0.0 commit

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ require('./models/Idea')
 require('./models/User')
 const {ensureAuthentication} = require('./Auth/auth')
 
-const port=process.env.port || 5000 
+const port=process.env.PORT || 5000 
 
 const app=express()
 
